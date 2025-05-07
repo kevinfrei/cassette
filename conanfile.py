3d47b425c4dfb8c2efa014f39cb060c5f2a89267
Kevin Frei <kevinfrei@hotmail.com>
2025-05-07 13:00:35 -0700

    Website + Crow's www stuff (okay, just cmake/conan)

---
+++ conanfile.py
@@ -0,0 +1,55 @@
+from conan import ConanFile
+from conan.tools.microsoft import vs_layout
+
+import os
+from typing import NamedTuple, Optional
+
+class CMakeInfo(NamedTuple):
+    # Name of the package to find with find_package
+    package: str
+    # CMake target to use with target_link_libraries, if needed
+    target: str
+    # CMake variable to use in CMakeLists, defaults to ucase "${package}_LIB"
+    var: Optional[str] = None
+
+class Library(NamedTuple):
+    # Name of library from conan-center
+    name: str
+    # Version of package from conan-center
+    version: str
+    # CMake package info
+    info: Optional[CMakeInfo] = None
+
+
+libraries = [
+    Library("boost", "1.83.0", CMakeInfo("Boost", "boost::boost")),
+    Library("crowcpp-crow", "1.0+3", CMakeInfo("Crow", "Crow::Crow")),
+    Library("gtest", "1.12.1", CMakeInfo("GTest", "gtest::gtest")),   
+    Library("libjpeg", "9e"),
+    Library("libpng", "1.6.40", CMakeInfo("PNG", "PNG::PNG")),
+    Library("libtiff", "4.6.0", CMakeInfo("TIFF", "TIFF::TIFF")),
+    Library("zlib", "1.3.1", CMakeInfo("ZLIB", "ZLIB::ZLIB")),
+]
+
+class CassetteRecipe(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def init(self):
+        # emit the find_package calls in the conanbuildinfo.cmake,
+        # along with the variables for later use in the CMakeLists
+        with open(os.path.join(self.recipe_folder, "ConanLibImports.cmake"), "w") as f:
+            f.write('# Generated file: DO NOT EDIT or COMMIT!\n')
+            f.write('# Add your library to the conanfile.py libraries list\n\n')
+            for library in libraries:
+                if library.info:
+                  f.write(f'find_package({library.info.package} REQUIRED)\n')
+                  var_name = library.info.var if library.info.var else f'{library.info.package.upper()}_LIB'
+                  f.write(f'set({var_name} {library.info.target})\n\n')
+                
+    def requirements(self):
+        for requirement in libraries:
+            self.requires(requirement.name+"/"+requirement.version)
+
+    def layout(self):
+        vs_layout(self)
