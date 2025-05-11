8f47c9444c7e95b559d7692456631a19ecc99ebf
Kevin Frei <kevinfrei@hotmail.com>
2025-05-11 16:17:31 -0700

    Clang formatting

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,32 +1,28 @@
 #define _WIN32_WINNT 0xA00
 
-#include <iostream>
 #include "crow.h"
+#include <iostream>
 
 int main(void) {
-    crow::SimpleApp app;
-    // Define a route
-    app.route<crow::black_magic::get_parameter_tag("/")>("/")([] {
-        return 
-            "<html>"
-                "<head>"
-                    "<title>Hi planet</title>"
-                "</head>"
-                "<body>"
-                    "<p>Hello, World!</p>"
-                "</body>"
-            "</html>";
-    });
-    std::cout << "http://localhost:18080" << std::endl;
+  crow::SimpleApp app;
+  // Define a route
+  app.route<crow::black_magic::get_parameter_tag("/")>("/")([] {
+    return "<html>"
+           "<head><title>Hi planet</title></head>"
+           "<body><p>Hello, World!</p></body>"
+           "</html>";
+  });
+  std::cout << "http://localhost:18080" << std::endl;
 #if defined(_WIN32)
-    system("start http://localhost:18080");
+  system("start http://localhost:18080");
 #elif defined(__APPLE__)
-    system("open http://localhost:18080");
+  system("open http://localhost:18080");
 #elif defined(__linux__)
-    system("xdg-open http://localhost:18080");
+  system("xdg-open http://localhost:18080");
 #else
-    std::cout << "Please open http://localhost:18080 in your browser." << std::endl;
+  std::cout << "Please open http://localhost:18080 in your browser."
+            << std::endl;
 #endif
-    app.port(18080).multithreaded().run();
-    return 0;
+  app.port(18080).multithreaded().run();
+  return 0;
 }
