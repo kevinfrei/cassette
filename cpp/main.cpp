e02ed8ed5ede13820a594df8d2b0ab8820b5b7e2
Kevin Frei <kevinfrei@hotmail.com>
2025-05-11 16:14:32 -0700

    Made it open the browswer automagically

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,6 +1,32 @@
+#define _WIN32_WINNT 0xA00
+
 #include <iostream>
+#include "crow.h"
 
 int main(void) {
-    std::cout << "Hello, World!" << std::endl;
+    crow::SimpleApp app;
+    // Define a route
+    app.route<crow::black_magic::get_parameter_tag("/")>("/")([] {
+        return 
+            "<html>"
+                "<head>"
+                    "<title>Hi planet</title>"
+                "</head>"
+                "<body>"
+                    "<p>Hello, World!</p>"
+                "</body>"
+            "</html>";
+    });
+    std::cout << "http://localhost:18080" << std::endl;
+#if defined(_WIN32)
+    system("start http://localhost:18080");
+#elif defined(__APPLE__)
+    system("open http://localhost:18080");
+#elif defined(__linux__)
+    system("xdg-open http://localhost:18080");
+#else
+    std::cout << "Please open http://localhost:18080 in your browser." << std::endl;
+#endif
+    app.port(18080).multithreaded().run();
     return 0;
 }
