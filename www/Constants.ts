91106def9c4ac518529ea8cb824f538444e13825
Kevin Frei <kevinfrei@hotmail.com>
2025-06-01 11:31:40 -0700

    Refactoring things into more sensible chunks

--- www/Constants.ts
+++ www/Constants.ts
@@ -1,7 +1,6 @@
 import {
   chkArrayOf,
   chkObjectOfType,
-  isNumber,
   isString,
   typecheck,
 } from '@freik/typechk';
@@ -12,8 +11,14 @@ import {
   IpcId_source,
   Keys_source,
   StrId_source,
-} from './SharedConstants';
+} from './Shared/Enums';
 
+// This is mostly just silly boilerplate to make sure that the enums
+// are properly typed and can be used in TypeScript.
+// I could generate this code from the gencpp.ts script, but I don't want to
+/*****
+ * Begin Boilerplate Code
+ *****/
 export const CurrentView = Object.freeze(CurrentView_source.values);
 export type CurrentViewEnum = (typeof CurrentView)[keyof typeof CurrentView];
 
@@ -27,29 +32,27 @@ export const IgnoreItemType = Object.freeze(IgnoreItemType_source.values);
 export type IgnoreItemTypeEnum =
   (typeof IgnoreItemType)[keyof typeof IgnoreItemType];
 
+// TODO: This stuff let's me localize my strings eventually
+export const StrId = Object.freeze(StrId_source.values);
+export type StrIdEnum = (typeof StrId)[keyof typeof StrId];
+
+/*****
+ * End Boilerplate Code
+ *****/
+
+export type IgnoreItem = { type: IgnoreItemTypeEnum; value: string };
+
 export const isIgnoreItemType: typecheck<IgnoreItemTypeEnum> = (
   val: unknown,
 ): val is IgnoreItemTypeEnum =>
   val === 'path-root' || val === 'path-keyword' || val === 'dir-name';
-export type IgnoreItem = { type: IgnoreItemTypeEnum; value: string };
+
 export const isIgnoreItem = chkObjectOfType<IgnoreItem>({
   type: isIgnoreItemType,
   value: isString,
 });
 export const isIgnoreItemArray = chkArrayOf<IgnoreItem>(isIgnoreItem);
 
-// TODO: This stuff let's me localize my strings eventually
-export const StrId = Object.freeze(StrId_source.values);
-export type StrIdEnum = (typeof StrId)[keyof typeof StrId];
-
 export function st(id: StrIdEnum): string {
   return id;
 }
-
-export const isCurrentView: typecheck<CurrentViewEnum> = (
-  val: unknown,
-): val is CurrentViewEnum =>
-  isNumber(val) &&
-  Number.isInteger(val) &&
-  val >= CurrentView.disabled &&
-  val <= CurrentView.tools;
