c3ea9d7e7845c0b7af594121da498574a6053770
Kevin Frei <kevinfrei@hotmail.com>
2025-07-12 20:51:59 -0700

    songListByKey for reals

--- www/Jotai/Songs.ts
+++ www/Jotai/Songs.ts
@@ -1,7 +1,10 @@
+import { isAlbumKey, isArtistKey, isSongKey } from '@freik/media-core';
 import { Fail } from '@freik/react-tools';
 import { atom, Atom } from 'jotai';
 import { atomFamily } from 'jotai/utils';
 import { AlbumKey, ArtistKey, MediaKey, SongKey } from 'www/Shared/CommonTypes';
+import { albumByKey } from './Albums';
+import { artistByKey } from './Artists';
 import { musicLibraryState } from './MusicLibrary';
 
 export const allSongsState = atom(
@@ -27,7 +30,22 @@ export const songListByKey = atomFamily<MediaKey, Atom<Promise<SongKey[]>>>(
     atom(async (get) => {
       if (key.length < 2) {
         return [];
+      } else if (isSongKey(key)) {
+        return [key];
+      } else if (isAlbumKey(key)) {
+        const alb = await get(albumByKey(key));
+        if (alb) {
+          return alb.songs;
+        } else {
+          return [];
+        }
+      } else if (isArtistKey(key)) {
+        const art = await get(artistByKey(key));
+        if (art) {
+          return art.songs;
+        } else {
+          return [];
+        }
       }
-      return [];
     }),
 );
