940d53ebf6851a1fd12b635094170091c7c54540
Kevin Frei <kevinfrei@hotmail.com>
2025-07-31 12:01:11 -0700

    Initial stuff working

--- cpp/include/audiofileindex.h
+++ cpp/include/audiofileindex.h
@@ -1,18 +1,51 @@
+#include <chrono>
+#include <cstdint>
 #include <filesystem>
+#include <functional>
+#include <optional>
+
+#include "CommonTypes.hpp"
 
 // Stuff for the afi implemention only?
 
 namespace afi {
 
 class audio_file_index {
-  static std::unordered_set<audio_file_index*> afis;
-
+  std::size_t hash;
   std::filesystem::path loc;
 
-  audio_file_index(const std::filesystem::path& loc);
-
  public:
-  static audio_file_index* create(const std::filesystem::path& loc);
+  using fspath = std::filesystem::path;
+  using path_handler = std::function<void(const fspath&)>;
+
+  audio_file_index(const fspath& loc, std::size_t hash = 0);
+
+  std::size_t getHash() const {
+    return hash;
+  }
+  const fspath& getLocation() const {
+    return loc;
+  }
+
+  std::optional<Shared::SongKey> makeSongKey(const fspath& songPath) const;
+  void forEachAudioFile(path_handler fn) const;
+  std::chrono::time_point<std::chrono::system_clock> getLastScanTime() const;
+  void rescanFiles(path_handler addAudioFile, path_handler delAudioFile);
+  void updateMetadata(const Shared::SimpleMetadata& newMetadata);
+  void updateMetadata(const Shared::FullMetadata& newMetadata);
+  std::optional<Shared::FullMetadata> getMetadataForSong(
+      const fspath& filePath) const;
+  void clearMetadataCache();
+  void clearLocalMetadataOverrides();
+  void setImageForSong(const Shared::SongKey& filePathOrKey,
+                       const std::vector<std::uint8_t>& buf);
+  std::optional<std::vector<std::uint8_t>> getImageForSong(
+      const Shared::SongKey& filePathOrKey, bool preferInternal = false) const;
+  void clearLocalImageCache();
+  void destroy();
+  void addIgnoreItem(Shared::IgnoreItemType which, const std::string& value);
+  bool removeIgnoreItem(Shared::IgnoreItemType which, const std::string& value);
+  std::vector<Shared::IgnoreItemPair> getIgnoreItems() const;
 };
 
 } // namespace afi
--- cpp/musicdb/audiofileindex.cpp
+++ cpp/musicdb/audiofileindex.cpp
@@ -11,8 +11,15 @@ namespace fs = std::filesystem;
 
 namespace afi {
 
-audio_file_index::audio_file_index(const std::filesystem::path& loc)
-    : loc(loc) {}
+audio_file_index::audio_file_index(const std::filesystem::path& _loc,
+                                   std::size_t _hash)
+    : hash(_hash), loc(std::filesystem::canonical(_loc)) {
+  if (hash == 0) {
+    // Compute hash based on the location, if not provided.
+    std::hash<std::string> hasher;
+    hash = hasher(loc.generic_string());
+  }
+}
 
 audio_file_index* audio_file_index::create(const std::filesystem::path& loc) {
   return new audio_file_index(loc);
