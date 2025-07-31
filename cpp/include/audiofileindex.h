af5737c5bd95ce2ff3dfb3f5c7effb6e725c7ae5
Kevin Frei <kevinfrei@hotmail.com>
2025-07-31 15:31:05 -0700

    A little bit of progress

--- cpp/include/audiofileindex.h
+++ cpp/include/audiofileindex.h
@@ -3,6 +3,7 @@
 #include <filesystem>
 #include <functional>
 #include <optional>
+#include <set>
 
 #include "CommonTypes.hpp"
 
@@ -13,6 +14,8 @@ namespace afi {
 class audio_file_index {
   std::size_t hash;
   std::filesystem::path loc;
+  std::chrono::time_point<std::chrono::system_clock> last_scan;
+  std::set<Shared::IgnoreItemPair> ignore_items;
 
  public:
   using fspath = std::filesystem::path;
@@ -23,29 +26,35 @@ class audio_file_index {
   std::size_t getHash() const {
     return hash;
   }
+
   const fspath& getLocation() const {
     return loc;
   }
 
+  // The simple stuff:
+  std::chrono::time_point<std::chrono::system_clock> getLastScanTime() const;
   std::optional<Shared::SongKey> makeSongKey(const fspath& songPath) const;
   void forEachAudioFile(path_handler fn) const;
-  std::chrono::time_point<std::chrono::system_clock> getLastScanTime() const;
   void rescanFiles(path_handler addAudioFile, path_handler delAudioFile);
+
+  // Metadata stuff:
   void updateMetadata(const Shared::SimpleMetadata& newMetadata);
   void updateMetadata(const Shared::FullMetadata& newMetadata);
   std::optional<Shared::FullMetadata> getMetadataForSong(
       const fspath& filePath) const;
   void clearMetadataCache();
   void clearLocalMetadataOverrides();
-  void setImageForSong(const Shared::SongKey& filePathOrKey,
+
+  void setImageForSong(const Shared::SongKey& songKey,
                        const std::vector<std::uint8_t>& buf);
+  bool clearImageForSong(const Shared::SongKey& songKey);
   std::optional<std::vector<std::uint8_t>> getImageForSong(
-      const Shared::SongKey& filePathOrKey, bool preferInternal = false) const;
+      const Shared::SongKey& songKey, bool preferInternal = false) const;
   void clearLocalImageCache();
-  void destroy();
+
   void addIgnoreItem(Shared::IgnoreItemType which, const std::string& value);
   bool removeIgnoreItem(Shared::IgnoreItemType which, const std::string& value);
-  std::vector<Shared::IgnoreItemPair> getIgnoreItems() const;
+  std::set<Shared::IgnoreItemPair> getIgnoreItems() const;
 };
 
 } // namespace afi
