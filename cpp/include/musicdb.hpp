c79c2d9b424c502511bb35aa524d57ba6aad947e
Kevin Frei <kevinfrei@hotmail.com>
2025-08-12 21:54:05 -0700

    The class definition for the Music Database is now in C++

--- cpp/include/musicdb.hpp
+++ cpp/include/musicdb.hpp
@@ -5,11 +5,69 @@
 #include <crow.h>
 
 #include "CommonTypes.hpp"
+#include "audiofileindex.h"
 
 namespace musicdb {
 
 Shared::MusicDatabase* get_music_db();
 void send_music_db(crow::websocket::connection& conn);
+
+class MusicDatabase {
+ public:
+  // Database API
+  Shared::SongWithPath getSong(Shared::SongKey& key);
+  Shared::Album getAlbum(Shared::AlbumKey& key);
+  Shared::Artist getArtist(Shared::ArtistKey& key);
+  Shared::SongKey getSongFromPath(std::string& filepath);
+  Shared::SearchResults searchIndex(bool substring, std::string& term);
+
+  // Full File Index stuff
+  bool addAudioFileIndex(const afi::audio_file_index& idx);
+  bool removeAudioFileIndex(const afi::audio_file_index& idx);
+
+  // "Implied" File Index stuff
+  bool addFileLocation(const std::string& str);
+  bool removeFileLocation(const std::string& str);
+  std::vector<std::string> getLocations() const;
+
+  // Pictures
+  void getAlbumPicture(const Shared::AlbumKey& key) const;
+  void setAlbumPicture(const Shared::AlbumKey& key, const void* filepath);
+  void getArtistPicture(const Shared::ArtistKey& key) const;
+  void setArtistPicture(const Shared::ArtistKey& key, const void* filepath);
+  void getSongPicture(const Shared::SongKey& key) const;
+  void setSongPicture(const Shared::SongKey& key, const void* filepath);
+  void clearImageCache();
+
+  // Not sure about exposing these three
+  // addSongFromPath(filepath: string): void; // Some Testing
+  // delSongByPath(filepath: string): boolean; // Some Testing
+  // delSongByKey(key: SongKey): boolean; // Some Testing
+
+  // Ignoring stuff:
+  void addIgnoreItem(Shared::IgnoreItemPair item);
+  bool removeIgnoreItem(Shared::IgnoreItemPair item);
+  std::vector<Shared::IgnoreItemPair> getIgnoreItems() const;
+
+  // For all the 'parsed' data
+  Shared::MusicDatabase getDatabase() const;
+
+  // Loading from/saving to persistence
+  bool load();
+  bool save();
+
+  // Updating
+  bool refresh();
+
+  // Metadata
+  bool updateMetadata(const std::string& fullPath,
+                      const Shared::FullMetadata& newMetadata);
+  Shared::FullMetadata getMetadata(const std::string& fullPathOrKey) const;
+  std::string getCanonicalFileName(const Shared::SongKey& song) const;
+  void clearMetadataCache();
+  void clearLocalMetadataOverrides();
+  // addOrUpdateSong(md: FullMetadata): void;
+};
 
 } // namespace musicdb
 
