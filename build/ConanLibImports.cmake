064f77adb31c9292241111a0fe67f17ff36d6e57
Kevin Frei <kevinfrei@hotmail.com>
2026-02-27 11:32:22 -0800

    Added platform folders v4.3.0, since it works

--- build/ConanLibImports.cmake
+++ build/ConanLibImports.cmake
@@ -10,14 +10,17 @@ set(CROW_LIB Crow::Crow)
 find_package(GTest REQUIRED)
 set(GTEST_LIB gtest::gtest)
 
+find_package(platform_folders REQUIRED)
+set(PLATFORMFOLDERS_LIB sago::platform_folders)
+
 find_package(portable-file-dialogs REQUIRED)
 set(PFD_LIB portable-file-dialogs::portable-file-dialogs)
 
-find_package(zstd REQUIRED)
-set(ZSTD_LIB zstd::libzstd_static)
+find_package(reproc++ REQUIRED)
+set(REPROC_LIB reproc::reproc)
 
 find_package(taglib REQUIRED)
 set(TAGLIB_LIB taglib::taglib)
 
-find_package(reproc++ REQUIRED)
-set(REPROC_LIB reproc::reproc)
+find_package(zstd REQUIRED)
+set(ZSTD_LIB zstd::libzstd_static)
